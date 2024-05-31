26172403d00fd48ebb43a31f9b7c30140bce0d90
Pawan Gola <pawangola908@gmail.com>
2024-05-31 22:32:59 +0530

    perform data types operation

---
+++ lib/data_types.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+void main(){
+ 
+  //int num1 = int.parse(stdin.readLineSync()!); 
+  print("1.Enter Your Name");
+  // String name = stdin.readLineSync()!;
+  //double value = double.parse(stdin.readLineSync()!); 
+    bool value = false; 
+    print(value); 
+  // print(name); 
+  // print(value); 
+  
+ 
+
+
+
+
+}
