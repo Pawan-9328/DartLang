8fcb2b0c5f5c8e05f5ba403f2d797448a5d434f3
Pawan Gola <pawangola908@gmail.com>
2024-05-31 22:01:46 +0530

    how to take input from user side

--- lib/test.dart
+++ lib/test.dart
@@ -1,8 +1,17 @@
+import 'dart:io';
 
- void main() {
+void main() {
+  //  print("Hiiiiii");
+  //  print("number");
+  // int num1 = 30 , num2 = 50;
+  //   int sum = num1+num2;
+  // //   print(sum);
+  //  stdout.write("is dart language");
 
-     print("Hiiiiii");    
-
-
-
- }
+  stdout.write("1.Enter Fist Number ");
+  int num1 = int.parse(stdin.readLineSync()!);
+  stdout.write("2.Enter second Number ");
+  int num2 = int.parse(stdin.readLineSync()!);
+  int sum = num1 + num2;
+  print(sum);
+}
