7b4c8fa2cbe4079a0f93c67b1a4878bf230a6e01
Pawan Gola <pawangola908@gmail.com>
2024-06-01 22:52:32 +0530

    construtors in dart

---
+++ lib/construtors.dart
@@ -0,0 +1,62 @@
+
+
+
+class Mathematics {
+  int n1 = 0;
+  int n2 = 0;
+
+  Mathematics(int n1, int n2) {
+    print("Object is created"); // constructor function
+    this.n1 = n1;
+    this.n2 = n2; 
+  }
+
+  int addition() {
+    return this.n1 + this.n2;
+  }
+
+  int subtraction() {
+    return this.n1 - this.n2;
+  }
+
+  int multiplication() {
+    return this.n1 * this.n2;
+  }
+
+  double division() {
+    return this.n1 / this.n2;
+  }
+}
+
+// class Addition {
+//   Addition() {
+//     stdout.write("Enter number");
+//     int num = int.parse(stdin.readLineSync()!);
+//   }
+
+//   sub() {
+//     print("Sub");
+//   }
+// }
+
+void main() {
+  // Addition obj = Addition();
+
+  void main() {
+    // Addition obj = Addition();
+    // obj.add();
+
+    Mathematics m1 = new Mathematics(34,17);
+    // int n1 = 34;
+    // int n2 = 17;
+
+    int addResult = m1.addition();
+    print(addResult);
+    int subResult = m1.subtraction();
+    print(subResult);
+    int mulResult = m1.multiplication();
+    print(mulResult);
+    double divResult = m1.division();
+    print(divResult);
+  }
+}
