04c5fe6b4932e7085a3e0fa9ca4517b19a94239e
Pawan Gola <pawangola908@gmail.com>
2024-06-02 01:31:15 +0530

    abstraction

---
+++ lib/abstract.dart
@@ -0,0 +1,19 @@
+void main() {
+  ElectronicsItem elec = ElectronicsItem();
+}
+
+abstract class ElectronicsItem {
+  void watching();
+
+  void watching1();
+
+
+  void fun1() {
+    print("Func1..");
+  }
+
+  void fun2() {
+    print("Func2..");
+  }
+}
+
