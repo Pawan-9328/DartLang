9cbb727f2a6ff235ae0eafc1010d3315373c1add
Pawan Gola <pawangola908@gmail.com>
2024-06-01 12:25:22 +0530

    list methods

---
+++ lib/list.dart
@@ -0,0 +1,18 @@
+void main() {
+  List<int> listNo = [1, 2, 3, 4];
+  // listNo.add(6);
+  // listNo.remove(6);
+  //listNo.removeAt(1);
+  // print(listNo.contains(6));
+  // print(listNo.length);
+  // print(listNo.first);
+  // print(listNo.last);
+  // listNo.insert(0, 12);
+  // print(listNo);
+  // listNo.insertAll(0, [5, 6, 7, 8]);
+  // print(listNo);
+  //reverse list
+  // print(listNo.reversed);
+  // print(listNo.isEmpty);
+  print(listNo.isNotEmpty); 
+}
